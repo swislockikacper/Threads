9adba6bc6e3cb96a37a55462f1261865a70af82b
swislockikacper <k.swislocki96@gmail.com>
2019-03-12 18:53:26 +0100

    added example

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads
+{
+    class Program
+    {
+        private static void PrintNumbers()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                    Thread.Sleep(200);
+                    Console.WriteLine($"Print in SECOND thread: {i}");
+            }
+        }
+
+        private static void PrintNumbers2()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(300);
+                Console.WriteLine($"Print in THIRD thread: {i}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var secondThreadStart = new ThreadStart(PrintNumbers);
+            var thirdThreadStart = new ThreadStart(PrintNumbers2);
+
+            var thread = new Thread(secondThreadStart);
+            thread.Start();
+
+            var thread2 = new Thread(thirdThreadStart);
+            thread2.Start();
+
+            Console.WriteLine("Main thread sleep (5 seconds)");
+            Thread.Sleep(5000);
+
+            Console.ReadKey();
+        }
+    }
+}
